6442751693a2993e80560eaf5dbc94a76749e7cb
omnibus1 <appleofeden8686@gmail.com>
2024-04-10 18:34:35 +0200

    created video model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class VideoModel(models.Model):
+    video = models.FileField(upload_to="videos")
+    date = models.DateTimeField(auto_now_add=True)
+    detection = models.CharField(max_length=255)
