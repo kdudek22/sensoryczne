443b75b24d1102ede42c23517f5a4cde9d2aa6af
omnibus1 <appleofeden8686@gmail.com>
2024-06-15 20:44:21 +0200

    added custom post to support a list of elements

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,7 @@
 from .serializers import VideoSerializer, ClassToPredictSerializer
 from .models import VideoModel, ClassToPredict
-from rest_framework.generics import ListCreateAPIView, ListAPIView,RetrieveUpdateDestroyAPIView
+from rest_framework.generics import ListCreateAPIView, ListAPIView,RetrieveUpdateDestroyAPIView, GenericAPIView
+from rest_framework.response import Response
 
 
 class VideoView(ListCreateAPIView):
@@ -8,9 +9,17 @@ class VideoView(ListCreateAPIView):
     serializer_class = VideoSerializer
 
 
-class ClassesToPredictListView(ListAPIView):
+class ClassesToPredictListView(ListAPIView, GenericAPIView):
     queryset = ClassToPredict.objects.all()
     serializer_class = ClassToPredictSerializer
+
+    def post(self, request, *args, **kwargs):
+        for entry in request.data:
+            obj = ClassToPredict.objects.get(name=entry["name"])
+            obj.is_active = entry["is_active"]
+            obj.save()
+
+        return Response(self.serializer_class(ClassToPredict.objects.all(), many=True).data)
 
 
 class ClassesToPredictView(RetrieveUpdateDestroyAPIView):
