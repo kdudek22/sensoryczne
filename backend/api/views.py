a21d2a5d813887b5d0e6bc8b17a5636e3bbf2a26
omnibus1 <appleofeden8686@gmail.com>
2024-06-15 17:05:31 +0200

    added new endpoint

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,18 @@
-from django.http import HttpResponse
-from .serializers import VideoSerializer
-from .models import VideoModel
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework.generics import ListCreateAPIView
+from .serializers import VideoSerializer, ClassToPredictSerializer
+from .models import VideoModel, ClassToPredict
+from rest_framework.generics import ListCreateAPIView, ListAPIView,RetrieveUpdateDestroyAPIView
 
 
 class VideoView(ListCreateAPIView):
     queryset = VideoModel.objects.all()
     serializer_class = VideoSerializer
+
+
+class ClassesToPredictListView(ListAPIView):
+    queryset = ClassToPredict.objects.all()
+    serializer_class = ClassToPredictSerializer
+
+
+class ClassesToPredictView(RetrieveUpdateDestroyAPIView):
+    serializer_class = ClassToPredictSerializer
+    queryset = ClassToPredict.objects.all()
