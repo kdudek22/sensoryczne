134f02c3b08fa5eca21ab500a2077bd0b5f90cff
omnibus1 <appleofeden8686@gmail.com>
2024-06-15 11:26:06 +0200

    basic setup

--- image_detection/brooker.py
+++ image_detection/brooker.py
@@ -1,29 +1,28 @@
 import paho.mqtt.client as mqtt
 import threading
 import json
+from logging_config import logger
 
 
 class BrokerClient:
-    def __init__(self, broker_address, topic, on_message):
+    def __init__(self, broker_address, read_topic, publish_topic):
         self.broker_address = broker_address
-        self.broker_topic = topic
+        self.broker_topic = read_topic
+        self.broker_publish_topic = publish_topic
 
-        self.client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1)
+        self.client = mqtt.Client()
 
         self.client.on_connect = self.on_connect
         self.client.on_message = self.on_message
-        self.message_callback = on_message
 
     def on_connect(self, client, userdata, flags, rc):
-        print("CLIENT - Connected with result code " + str(rc))
+        logger.info("Connected with result code " + str(rc))
         client.subscribe(self.broker_topic)
 
     def on_message(self, client, userdata, msg):
-        print("CLIENT - Received message on topic: " + msg.topic + " - Message " + str(msg.payload.decode()))
-
-        msg_json = json.loads(msg.payload.decode())
-
-        self.message_callback(msg_json)
+        logger.info("Received message on topic: " + msg.topic + " - Message " + str(msg.payload.decode()))
+        # msg_json = json.loads(msg.payload.decode())
+        # self.message_callback(msg_json)
 
     def connect(self):
         self.client.connect(self.broker_address, 1883, 60)
@@ -38,5 +37,4 @@ class BrokerClient:
         self.client.loop_forever()
 
     def publish(self, message):
-        """#TODO think if we want to send messages back?"""
-        pass
+        self.client.publish(self.broker_publish_topic, message)
