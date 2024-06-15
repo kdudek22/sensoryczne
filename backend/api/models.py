82d79a3d240b1fe14c54d09c04076adb39536f1f
omnibus1 <appleofeden8686@gmail.com>
2024-06-15 16:33:38 +0200

    added new model

--- backend/api/models.py
+++ backend/api/models.py
@@ -5,3 +5,8 @@ class VideoModel(models.Model):
     video = models.FileField(upload_to="videos")
     date = models.DateTimeField(auto_now_add=True)
     detection = models.CharField(max_length=255)
+
+
+class ClassToPredict(models.Model):
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=False)
