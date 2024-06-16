efc584fa005a51840594d0d1d0dce163513b2aa0
omnibus1 <appleofeden8686@gmail.com>
2024-06-16 22:40:41 +0200

    updated endpoint to send broker events

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,7 +2,8 @@ from .serializers import VideoSerializer, ClassToPredictSerializer
 from .models import VideoModel, ClassToPredict
 from rest_framework.generics import ListCreateAPIView, ListAPIView,RetrieveUpdateDestroyAPIView, GenericAPIView
 from rest_framework.response import Response
-
+from .broker import BrokerClient
+import json
 
 class VideoView(ListCreateAPIView):
     queryset = VideoModel.objects.all().order_by("-date")
@@ -14,11 +15,20 @@ class ClassesToPredictListView(ListAPIView, GenericAPIView):
     serializer_class = ClassToPredictSerializer
 
     def post(self, request, *args, **kwargs):
+
         for entry in request.data:
             if ClassToPredict.objects.filter(name=entry["name"]).exists():
                 obj = ClassToPredict.objects.get(name=entry["name"])
                 obj.is_active = entry["is_active"]
                 obj.save()
+
+        active_classes = [c.name for c in ClassToPredict.objects.filter(is_active=True)]
+        broker = BrokerClient("34.116.207.218", "", "test/detection_settings")
+        broker.connect()
+
+        broker.publish(json.dumps({"classes_to_predict": active_classes}))
+
+        broker.client.disconnect()
 
         return Response(self.serializer_class(ClassToPredict.objects.all(), many=True).data)
 
